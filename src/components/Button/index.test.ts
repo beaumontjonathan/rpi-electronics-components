e6fa6e2508f4e409d2b13c6a0c62662bac023a1e
beaumontjonathan <beaumont_jonny@hotmail.co.uk>
2019-03-27 00:01:05 +0000

    Added test script, fixed tests

--- src/components/Button/index.test.ts
+++ src/components/Button/index.test.ts
@@ -18,28 +18,38 @@ describe('Button', () => {
     mockWatch.mockReset();
   });
 
-  it('should watch', async () => {
-    expect(typeof b.onClick().then).toBe('function');
-    expect(mockWatch).toHaveBeenCalled();
+  describe('#getPin', () => {
+    it('should match the provided pin', () => {
+      const pinId = 6;
+      const button = new Button({ pinId });
+      expect(button.getPin()).toBe(pinId);
+    });
   });
 
-  it('should resolve onClick promise when watch called', async () => {
-    const promise = b.onClick();
-    expect(mockWatch).toHaveBeenCalled();
-    const [watch] = getLatestCall();
-    watch(null, 1);
-    await expect(promise).resolves.toBe(undefined);
-  });
+  describe('#onClick', () => {
+    it('should watch', async () => {
+      expect(typeof b.onClick().then).toBe('function');
+      expect(mockWatch).toHaveBeenCalled();
+    });
 
-  it('should debounce', async () => {
-    const debounce = 500;
-    const p1 = b.onClick({ debounce });
-    expect(mockWatch).toHaveBeenCalled();
-    const [watch] = getLatestCall();
-    const start = Date.now();
-    watch(null, 1);
-    await p1;
-    const end = Date.now();
-    expect(end - start).toBeGreaterThanOrEqual(debounce);
+    it('should resolve onClick promise when watch called', async () => {
+      const promise = b.onClick();
+      expect(mockWatch).toHaveBeenCalled();
+      const [watch] = getLatestCall();
+      watch(null, 1);
+      await expect(promise).resolves.toBe(undefined);
+    });
+
+    it('should debounce', async () => {
+      const debounce = 100;
+      const p1 = b.onClick({ debounce });
+      expect(mockWatch).toHaveBeenCalled();
+      const [watch] = getLatestCall();
+      const start = Date.now();
+      watch(null, 1);
+      await p1;
+      const end = Date.now();
+      expect(end - start).toBeGreaterThanOrEqual(debounce);
+    });
   });
 });
