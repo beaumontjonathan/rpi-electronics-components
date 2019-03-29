00fc8edb55342d8443f7fac38d5d29423231b7f9
beaumontjonathan <beaumont_jonny@hotmail.co.uk>
2019-03-29 19:37:47 +0000

    Build commit

---
+++ dist/components/Lcd/index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.Lcd = void 0;
+
+class Lcd {
+  constructor() {
+    console.log('Placeholder class...');
+  }
+
+}
+
+exports.Lcd = Lcd;
+//# sourceMappingURL=index.js.map
