afa845f8d5b83a087eda982515d315e41841195a
Jonathan Beaumont <beaumont_jonny@hotmail.co.uk>
2019-03-31 20:44:20 +0100

    Feature add lcd (#2)
    * Added lcd declarations
    
    * Wrapped lcd

--- src/components/Lcd/index.ts
+++ src/components/Lcd/index.ts
@@ -1,5 +1,131 @@
+import InternalLcd from 'lcd';
+
+export interface LcdPins {
+  rs: number,
+  e: number,
+  data: [number, number, number, number],
+}
+
+export interface LcdArgs extends LcdPins {
+  cols: number,
+  rows: number,
+}
+
+export interface ColRow {
+  col: number,
+  row: number,
+}
+
 export class Lcd {
-  constructor() {
-    console.log('Placeholder class...')
+  private readonly args: LcdArgs;
+  private readonly internalLcd: InternalLcd;
+
+  constructor(args: LcdArgs) {
+    this.internalLcd = new InternalLcd(args);
+  }
+
+  public at({ col, row }: ColRow): this {
+    this.internalLcd.setCursor(col, row);
+    return this;
+  }
+
+  public print(str: string): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      this.internalLcd.print(str, (err) => {
+        if (err) {
+          return reject(err);
+        }
+
+        resolve();
+      });
+    });
+  }
+
+  public printAt(str: string, { col, row }: ColRow): Promise<void> {
+    return this.at({ col, row }).print(str);
+  }
+
+  public clear(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.internalLcd.clear(err => {
+        if (err) {
+          return reject(err);
+        }
+
+        resolve();
+      })
+    });
+  }
+
+  public home(): Promise<void> {
+    return new Promise((resolve, reject) => {
+      this.internalLcd.home(err => {
+        if (err) {
+          return reject(err);
+        }
+
+        resolve();
+      })
+    });
+  }
+
+  public showCursor(): void {
+    this.internalLcd.cursor();
+  }
+
+  public hideCursor(): void {
+    this.internalLcd.noCursor();
+  }
+
+  public showBlink(): void {
+    this.internalLcd.blink();
+  }
+
+  public scrollDisplayLeft(): void {
+    this.internalLcd.scrollDisplayLeft();
+  }
+
+  public hideBlink(): void {
+    this.internalLcd.noBlink();
+  }
+
+  public scrollDisplayRight(): void {
+    this.internalLcd.scrollDisplayRight();
+  }
+
+  public leftToRight(): void {
+    this.internalLcd.leftToRight();
+  }
+
+  public rightToLeft(): void {
+    this.internalLcd.rightToLeft();
+  }
+
+  public autoScrollOn(): void {
+    this.internalLcd.autoscroll();
+  }
+
+  public autoScrollOff(): void {
+    this.internalLcd.noAutoscroll();
+  }
+
+  public close(): void {
+    this.internalLcd.close();
+  }
+
+  public getPins(): LcdPins {
+    const { rs, e, data } = this.args;
+
+    return { rs, e, data };
+  }
+
+  public getPinCodes(): [number, number, number, number, number, number] {
+    const { rs, e, data: [d1, d2, d3, d4] } = this.args;
+
+    return [rs, e, d1, d2, d3, d4];
+  }
+
+  public releasePins(): void {
+    this.internalLcd.close();
   }
 }
--- src/usage.js
+++ src/usage.js
@@ -1,1 +1,35 @@
-import { Button } from ''
+import { Button, Lcd } from '.'
+
+const run = async () => {
+  const button = new Button({ pinId: 4 });
+  const lcd = new Lcd({
+    rs: 1,
+    e: 2,
+    data: [3, 4, 5, 6],
+    cols: 16,
+    rows: 2,
+  });
+
+  let n = 0;
+
+  await lcd.at({ row: 0, col: 0 }).print('Press the button');
+
+  try {
+    while (true) {
+      await Promise.all([
+        lcd.at({ row: 1, col: 0 }).print(` n = ${n}`),
+        button.onClick({ debounce: 100 }),
+      ]);
+    }
+  } catch ({ message }) {
+    await lcd.at({ row: 0, col: 0 }).print(' Error occurred ');
+    await lcd.at({ row: 1, col: 9 }).print(message.substr(0, 12));
+
+    button.releasePin();
+    lcd.releasePins();
+  }
+};
+
+run()
+  .then(console.log)
+  .catch(console.log);
